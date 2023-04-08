0db9b380c143045aa028cf5ba4d96e77f1b05137
MirabdullaYuldashev <mirabdullayuldashev@gmail.com>
2023-04-08 10:35:09 +0600

    Home work 4

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function RegistrationForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        phoneNumber: '',
+        birthYear: ''
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(JSON.stringify(formData));
+        console.log(formData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Имя:</label>
+                <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
+            </div>
+            <div>
+                <label htmlFor="phoneNumber">Телефон:</label>
+                <input type="tel" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} />
+            </div>
+            <div>
+                <label htmlFor="birthYear">Год рождения:</label>
+                <input type="number" id="birthYear" name="birthYear" value={formData.birthYear} onChange={handleInputChange} />
+            </div>
+            <button type="submit">Отправить</button>
+        </form>
+    );
+}
+
+export default RegistrationForm;
